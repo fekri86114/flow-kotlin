a497bfdd1a31a227bc7982db41daffa68e63b77e
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-06-11 01:22:29 -0700

    added dependencies | flow simple example

--- app/src/main/java/info/fekri/flow/MainActivity.kt
+++ app/src/main/java/info/fekri/flow/MainActivity.kt
@@ -1,8 +1,11 @@
 package info.fekri.flow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import info.fekri.flow.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val viewModel = MainViewModel()
+
+        lifecycleScope.launch {
+            viewModel.createFlow.collect {
+                Log.v("TEST_FLOW_LOG", it.toString())
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/info/fekri/flow/MainViewModel.kt
@@ -0,0 +1,17 @@
+package info.fekri.flow
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class MainViewModel {
+
+    val createFlow: Flow<Int> = flow {
+        while (true) {
+            emit((1000..10000).random())
+            delay(100)
+        }
+    }
+
+}
